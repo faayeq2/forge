087a71136427072789eb18cb95fe0442619b6866
Muhammad Faayeq Ali Khan <faayeqali@gmail.com>
2025-05-15 03:52:16 +0500

    testing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # forge
 This is an implementation of a git commit generator in Go.
+
+requirements:
+go get github.com/manifoldco/promptui
--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,38 @@ package main
 import (
 	"fmt"
 	"forge/internal/config"
+	"os"
+	"os/exec"
+
+	"github.com/manifoldco/promptui"
 	//"os/exec"
 )
 
-func disp_verbs() {
-	for i := 0; i < len(config.Gitverbs); i++ {
-		fmt.Println(config.Gitverbs[i])
+func user_input() {
+	fmt.Println("type of commit: ")
+	comm_type := promptui.Select{
+		Label: "commit type:",
+		Items: config.Gitverbs,
 	}
+
+	comm_type.Run()
+
+	fmt.Println("")
+
+	fmt.Print("enter commit message")
+	var msg string
+	fmt.Scanln(&msg)
+	cmd := exec.Command("git", "commit", "-m", msg)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("git commit failed: ", err)
+		return
+	}
+	fmt.Println("commit succesful")
 }
 
 func main() {
-	disp_verbs()
-	//cmd := exec.Command(str)
-	//cmd.Run()
+	user_input()
 }
